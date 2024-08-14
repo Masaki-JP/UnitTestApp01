4ba81d97875922baac6160a0e909f544572a8cb0
Masaki-JP <masakidayo0508@gmail.com>
2024-08-14 23:06:11 +0900

    Initial Commit

---
+++ UnitTestApp01/UnitTestApp01App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestApp01App.swift
+//  UnitTestApp01
+//
+//  Created by Masaki Doi on 2024/08/14.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestApp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
